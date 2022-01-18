3b4f32edd3d67152b1309b2469f87f6028b19452
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-01-19 01:22:49 +0530

    Time: 640 ms (30.68%), Space: 167.6 MB (6.81%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-   long long mostPoints(vector<vector<int>>& questions) {
-        int n=questions.size();
-        long long ans=0;
-        vector<pair<long long,long long>> dp(n);
-        for(int i=n-1;i>=0;i--){
-            int x=i+questions[i][1]+1;
-            if(x<n) dp[i].first= questions[i][0] + max(dp[x].first,dp[x].second); 
-            else dp[i].first=questions[i][0];
-            ans=max(ans,dp[i].first);
-            dp[i].second=ans;
+   
+    long long points(vector<vector<int>>& questions,vector<long long> &vec,int i,int &n){
+        // base case
+		if(i>=n) return 0;
+		// if we already computed for ith index, return vec[i]
+        if(vec[i]!=0) return vec[i];
+        else{
+		// case1 : If we want to take ith question
+            long long take=questions[i][0]+points(questions,vec,i+questions[i][1]+1,n);
+			// case2: If we don't want to take ith question
+            long long ntake=points(questions,vec,i+1,n);
+            vec[i]= max(take,ntake);
+            return vec[i];
         }
-        return ans;
+    }
+
+    long long mostPoints(vector<vector<int>>& questions) {
+	// for memoization
+        vector<long long> vec(100001,0);
+        int n=questions.size();
+        return points(questions,vec,0,n); 
     }
 };
