37ed84888b4dcfacaef036ecf626303440619ebc
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-01-21 14:13:08 +0530

    Time: 124 ms, Memory: 14.5 MB - LeetHub

---
+++ coin-change/coin-change.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public: 
+   
+    int coinChange(vector<int>& coins, int amount) {
+       vector<int> dp(amount + 1, -1);
+        dp[0] = 0;
+        
+        for (int i = 1; i <= amount; ++i)
+            for (auto & c : coins)
+                if (i - c >= 0 && dp[i - c] != -1)
+                    dp[i] = dp[i] > 0 ? min(dp[i], dp[i - c] + 1) : dp[i - c] + 1;
+        
+        return dp[amount];  
+    }
+};
