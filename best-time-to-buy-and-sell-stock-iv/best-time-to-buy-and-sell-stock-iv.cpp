a441f2fe21ea3f8e8b1e6052ab8d00ccfe72ec20
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-01-21 15:18:11 +0530

    Time: 4 ms, Memory: 10.7 MB - LeetHub

--- best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -3,13 +3,13 @@ public:
     int maxProfit(int k, vector<int>& prices) {
          int len = prices.size();
         if (len<2) return 0;
-        if (k>len/2){ // simple case
+       /* if (k>len/2){ // simple case
             int ans = 0;
             for (int i=1; i<len; ++i){
                 ans += max(prices[i] - prices[i-1],0);
             }
             return ans;
-        }
+        }*/
         int hold[k+1];
         int rele[k+1];
         for (int i=0;i<=k;++i){
