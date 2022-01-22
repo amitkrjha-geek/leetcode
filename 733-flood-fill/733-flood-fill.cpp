a5245526bd0b264228b289eb109c7ac7f78c0469
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-01-22 14:42:23 +0530

    Time: 14 ms (32.27%), Space: 17.3 MB (7.45%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int color = -1;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        if(color == -1) color = image[sr][sc];
+      //validate
+        if(image.empty() || sr == -1 || sr == image.size() || sc == -1
+           || sc == image[0].size() || image[sr][sc] != color || color == newColor) 
+            return image;
+        //dfs
+        image[sr][sc] = newColor;
+        floodFill(image, sr - 1, sc, newColor);
+        floodFill(image, sr + 1, sc, newColor);
+        floodFill(image, sr, sc - 1, newColor);
+        floodFill(image, sr, sc + 1, newColor);
+        return image;
+    }
+};
