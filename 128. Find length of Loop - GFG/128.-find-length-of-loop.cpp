b3286bd157b28618eb0239b56dad7867867e532e
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-05-11 15:22:13 +0530

    Added solution - LeetHub

---
+++ 128. Find length of Loop - GFG/128.-find-length-of-loop.cpp
@@ -0,0 +1,101 @@
+// { Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+int countNodesinLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        cout<< countNodesinLoop(head) << endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+//Function to find the length of a loop in the linked list.
+int countNodesinLoop(struct Node *head)
+{
+    // Code here
+    struct Node *slow=head,*fast=head,*entry=head;
+    bool cycle=false;int count=1;
+    
+    while(fast&&fast->next)
+    {
+        fast=fast->next->next;
+        slow=slow->next;
+        if(slow==fast)
+        {   //cout<<1;
+            cycle=true;
+            slow=slow->next;
+            while(fast!=slow)
+            {  
+                slow=slow->next;
+                
+                count++;
+            }
+            break;
+        }
+    }
+       if(!cycle)return 0;
+        return count;
+    }
