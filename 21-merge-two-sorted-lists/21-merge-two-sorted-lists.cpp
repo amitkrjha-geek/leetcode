5fd63e0c2542304e9d18405060dae1f067969824
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-05-12 14:23:09 +0530

    Time: 15 ms (25.50%), Space: 14.8 MB (82.52%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode dummy(INT_MIN);
+                 ListNode dummy(INT_MIN);
         ListNode *tail = &dummy;
         
         while (l1 && l2) {
