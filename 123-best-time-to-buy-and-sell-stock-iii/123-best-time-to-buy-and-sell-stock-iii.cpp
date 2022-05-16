5a862cf5b206747e42a553b7ad96a6242bc625e1
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-05-16 23:18:06 +0530

    Time: 1646 ms (5.00%), Space: 281.9 MB (5.12%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
+        int pp(int i, int hold, int k, vector<vector<vector<int>>> & dp, vector<int>& p)
+      {
+        if(i==p.size() || k==0) return 0;
+        if(dp[i][k][hold]!=-1) return dp[i][k][hold];
+        int noti=pp(i+1,hold,k,dp,p);
+        if(hold==1)
+        {
+            int sell=pp(i+1,0,k-1,dp,p);
+            dp[i][k][hold]=max(sell+p[i],noti);
+        }
+        else
+        {
+            int buy=pp(i+1,1,k,dp,p);
+            dp[i][k][hold]=max(buy-p[i],noti);
+        }
+        return dp[i][k][hold];
+    }
     int maxProfit(vector<int>& prices) {
-        if(!prices.size())
-            return 0;
-        int n = prices.size();
-        int left[n], right[n];
-        int leftmin = prices[0], rightmax = prices[n-1], maxprofit = 0;
-        left[0] = 0; right[n - 1] = 0;                                     
-        int i,j;
-        for(i = 1, j = n-2; i < n, j >= 0; i++, j--){
-            leftmin  = min(leftmin, prices[i]);                            
-            left[i]  = max(left[i - 1], prices[i] - leftmin);              
-            rightmax = max(rightmax, prices[j]);                          
-            right[j] = max(right[j + 1], rightmax - prices[j]);           
-        }
-        for(int i = 0; i < n; i++){
-            maxprofit = max(maxprofit, left[i] + right[i]);
-        }
-        return maxprofit;
+        int n=prices.size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(3,vector<int>(2,-1)));
+        return pp(0,0,2,dp,prices);
     }
 };
