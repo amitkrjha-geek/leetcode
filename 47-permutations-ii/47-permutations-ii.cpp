d9e2acc0c98cbd2ec10620168585aa59c32f5996
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-05-20 22:25:50 +0530

    Time: 13 ms (52.82%), Space: 9.1 MB (66.51%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,21 +1,20 @@
 class Solution {
-
-    public:
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> output;
-        sort(begin(nums), end(nums));
-        generatePermutations(nums, output, 0);
-        return output;
+public:
+    void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
+        if (i == j-1) {
+            res.push_back(num);
+            return;
+        }
+        for (int k = i; k < j; k++) {
+            if (i != k && num[i] == num[k]) continue;
+            swap(num[i], num[k]);
+            recursion(num, i+1, j, res);
+        }
     }
-private:
-    void generatePermutations(vector<int> nums, vector<vector<int>>& output, int idx) {
-        if (idx == size(nums)) {
-            output.emplace_back(nums);
-        }
-        for (int i = idx; i < size(nums); ++i) {
-            if (i != idx && nums[i] == nums[idx]) continue;
-            swap(nums[i], nums[idx]);
-            generatePermutations(nums, output, idx + 1);
-        }
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int> >res;
+        recursion(num, 0, num.size(), res);
+        return res;
     }
 };
