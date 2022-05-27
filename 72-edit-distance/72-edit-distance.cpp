0e36479cdcad131dab8f7a6e8ea7285759ad3eb2
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-05-27 13:01:09 +0530

    Time: 3 ms (99.09%), Space: 7.2 MB (90.46%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-     int dp[1001][1001];
-    int minDist(string &a, string &b, int i, int j) {
-         if (i == -1) {
-            return j + 1;
-        }
-        if (j == -1) {
-            return i + 1;
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        if (a[i] == b[j]) {
-            dp[i][j] =  minDist(a, b, i - 1, j - 1);
-            return dp[i][j];
-        }
-        else {
-            int add = minDist(a, b, i, j - 1);
-            int replace = minDist(a, b, i - 1, j - 1);
-            int remove = minDist(a, b, i - 1, j);
-            
-            dp[i][j] =  1 + min(min(add, replace), remove);
-            return dp[i][j];
-        }
-    }
-
     int minDistance(string word1, string word2) {
-        memset(dp,-1,sizeof(dp));
-        
-        return minDist(word1,word2,word1.size()-1,word2.size()-1);
+          int n= word1.size();
+        int m= word2.size();
+        int dp[n+1][m+1];
+        for(int i=0;i<=m;i++){
+            dp[0][i]= i;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]) dp[i][j]= dp[i-1][j-1];
+                else {
+                    dp[i][j]= 1 + min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
