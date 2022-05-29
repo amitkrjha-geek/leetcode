595acd079783fbb6b550ffca1d8fc44532c69aaa
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-05-29 15:31:39 +0530

    Time: 441 ms (17.03%), Space: 23 MB (34.38%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     
-    int dp[1002];
+//     int dp[1002];
     
-    int recurs(vector<vector<int>>& pairs,int i)
-    {
-        if(i>=pairs.size())
-        {
-            return 0;
-        }
+//     int recurs(vector<vector<int>>& pairs,int i)
+//     {
+//         if(i>=pairs.size())
+//         {
+//             return 0;
+//         }
         
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
+//         if(dp[i]!=-1)
+//         {
+//             return dp[i];
+//         }
         
-        int ans=0;
-        for(int j=i+1;j<pairs.size();j++)
-        {
-            if(pairs[i][1]<pairs[j][0])
-            {
-                ans=max(ans,recurs(pairs,j)+1);
-            }
-             ans=max(ans,recurs(pairs,j));
-        }
-            return dp[i]= ans;
+//         int ans=0;
+//         for(int j=i+1;j<pairs.size();j++)
+//         {
+//             if(pairs[i][1]<pairs[j][0])
+//             {
+//                 ans=max(ans,recurs(pairs,j)+1);
+//             }
+//              ans=max(ans,recurs(pairs,j));
+//         }
+//             return dp[i]= ans;
         
-    }
+//     }
    
     
     
@@ -34,19 +34,36 @@ public:
     
     int findLongestChain(vector<vector<int>>& pairs) {
         
-        for(int i=0;i<=pairs.size();i++)
-        {
-            dp[i]=-1;
+//         for(int i=0;i<=pairs.size();i++)
+//         {
+//             dp[i]=-1;
+//         }
+        
+//         sort(pairs.begin(),pairs.end());
+        
+//         int ans=0;
+//         for(int i=0;i<pairs.size();i++)
+//         {
+//             ans=max(ans,recurs(pairs,i));
+//         }
+        
+//         return ans+1;
+          sort(pairs.begin(), pairs.end());
+        int n=pairs.size();
+        vector<int>lis(n+1);
+        lis[0]=1;
+        for(int i=1;i<n;i++)
+        {  int ans=0;
+            for(int j=0;j<i;j++)
+            { if(pairs[i][0]>pairs[j][1])
+                {
+                    ans=max(ans,lis[j]+1);
+                }
+            else ans=max(ans,lis[j]);
+            }
+         lis[i]=ans;
+         
         }
-        
-        sort(pairs.begin(),pairs.end());
-        
-        int ans=0;
-        for(int i=0;i<pairs.size();i++)
-        {
-            ans=max(ans,recurs(pairs,i));
-        }
-        
-        return ans+1;
+        return *max_element(lis.begin(),lis.end());
     }
 };
