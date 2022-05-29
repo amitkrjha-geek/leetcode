8ee81aa6948b547bc0fec5f3361156f25bce7bac
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-05-30 01:26:33 +0530

    Time: 11 ms (5.01%), Space: 8.4 MB (24.89%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int r=0, w=0, b=0; // label the end of different colors;
+           int r=0, w=0, b=0; 
     for(int num: nums){
         if(num==0) {nums[b++]=2; nums[w++]=1; nums[r++]=0; } 
         else if(num==1) {nums[b++]=2; nums[w++]=1;}
