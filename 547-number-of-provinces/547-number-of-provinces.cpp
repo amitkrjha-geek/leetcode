ca786d9de62c560e564f7cf02d0c7aa52a447f6e
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-06-08 20:28:43 +0530

    Time: 34 ms (48.23%), Space: 13.8 MB (42.54%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    
+    vector<int> v;
+    
+   
+    int parent(int x)
+    {
+        if(v[x]==-1) return x; 
+        return v[x]=parent(v[x]);
+    }
+    
+    void _union(int a,int b)
+    {
+        int p_a=parent(a),p_b=parent(b);
+        
+        if(p_a==p_b) return; 
+        
+        v[p_a]=p_b;
+    }
+    
+    int findCircleNum(vector<vector<int>>& M) { 
+        int n=M.size();
+        v=vector<int> (n,-1);
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(M[i][j])  
+                { 
+                    _union(i,j);  
+                }
+            }
+        }
+        int c=0; 
+        
+        for(int i=0;i<n;i++)
+        {
+            if(v[i]==-1) c++; 
+        }
+        return c; 
+    }
+};
