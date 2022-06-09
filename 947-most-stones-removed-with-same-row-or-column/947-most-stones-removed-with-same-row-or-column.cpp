86eeecc800f446619e7ea4552a0919a40f83f5ef
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-06-09 12:01:25 +0530

    Time: 360 ms (25.89%), Space: 14.2 MB (82.91%) - LeetHub

---
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,40 @@
+
+class Solution {
+public:
+    vector<int> parent;         
+    
+    int find(int node){                   
+        if(node==parent[node]) return node;
+        return parent[node] = find(parent[node]);
+    }
+    
+    bool join(int x, int y){
+        x = find(x);
+        y = find(y);
+        
+        if(x==y) return false;
+        
+        parent[y]=x;
+        return true;
+    }
+    
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        parent.resize(n);
+        
+        for(int i=0; i<n; i++){
+            parent[i] = i;
+        }
+        
+        int count=0;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1]){
+                    if(join(i,j)) count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
