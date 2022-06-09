a8b56cb5966f7ee448ab5d4ad7f1c53e4f0f7b44
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-06-09 13:18:07 +0530

    Time: 166 ms (66.94%), Space: 37.5 MB (93.29%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,30 +1,29 @@
 class Solution {
+    vector<int>parent;
+    int find(int x) {
+        return (parent[x] == x) ? x : find(parent[x]);
+    }
 public:
-     void dfs(vector<vector<int>> &adj, vector<bool> &visited, int src)
-    {
-        visited[src] = true;
-        for(int i : adj[src])
-            if(!visited[i])
-                dfs(adj, visited, i);
-    }
     int makeConnected(int n, vector<vector<int>>& connections) {
-         if(connections.size() < n - 1)
-            return -1;
-        vector<vector<int>> adj(n);
-        for(auto v : connections)
-        {
-            adj[v[0]].push_back(v[1]);
-            adj[v[1]].push_back(v[0]);
+        
+        int c = connections.size();
+        if (n-1 > c) return -1;
+        
+        parent.resize(n);
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+        
+        for (int i = 0; i < c; i++) {
+            int x = find(connections[i][0]);
+            int y = find(connections[i][1]);
+            if (x != y) parent[y] = x;
         }
-        vector<bool> visited(n, false);
-        int components = 0;
-        for(int i=0; i<n; i++)
-            if(!visited[i])
-            {
-                dfs(adj, visited, i);
-                components++;
-            }
-        return components - 1;
+        
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+            if (parent[i] == i)
+                ans++;
+        
+        return ans-1;
     }
-
 };
