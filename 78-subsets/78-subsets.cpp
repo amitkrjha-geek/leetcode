b1a21e946bed1ba10cb4bb8d121f615a3c542a5a
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-06-13 11:32:01 +0530

    Time: 8 ms (20.81%), Space: 7.3 MB (54.59%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>>res;
-    void f(int i,vector<int>&nums,vector<int>&path)
-    {
-        res.push_back(path);
-        for(int j=i;j<nums.size();j++)
-        {
-            path.push_back(nums[j]);
-            f(j+1,nums,path);
-            path.pop_back();
-        }
+   vector<vector<int>>res;
+//     void f(int i,vector<int>&nums,vector<int>&path)
+//     {
+//         res.push_back(path);
+//         for(int j=i;j<nums.size();j++)
+//         {
+//             path.push_back(nums[j]);
+//             f(j+1,nums,path);
+//             path.pop_back();
+//         }
        
-    }
+//     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>path;
-        f(0,nums,path);
+       // vector<int>path;
+      //  f(0,nums,path);
+       // return res;
+       // res.push_back({});
+        int n=nums.size();
+        for(int i=0;i<(1<<n);i++)
+        {  vector<int>v;
+            for(int j=0;j<n;j++)
+            {
+                if((i>>j)&1)
+                    v.push_back(nums[j]);
+            }
+         res.push_back(v);
+         v.clear();
+        }
         return res;
     }
 };
