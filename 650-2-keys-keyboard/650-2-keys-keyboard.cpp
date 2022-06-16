b6227481320ea0f2cc90a0dbb40e6f21f5a8605b
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-06-16 15:54:30 +0530

    Time: 3 ms (78.12%), Space: 5.9 MB (90.59%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
-   #define vi vector<int>
-#define vvi vector<vi>
-#define INF 1'000'000
-         
-        int count( int i, int n, int l, vvi& dp )
-        {
-            if( i > n )
-                return INF ;
-				
-            else if(!(n-i))
-                return 0 ;
-				
-            else if( dp[i][l] )
-                return dp[i][l] ;
-				
-            else
-            {
-                int copy  = 2 + count(i+i,n,i,dp); 
-                int paste = 1 + count(i+l,n,l,dp);
-				
-                return dp[i][l] = min( copy, paste );
-            }
+    int minSteps(int n) {
+        int res = 0;
+    for(int i=2;i<=n;i++){
+        while(n%i == 0){
+            res+= i;
+            n=n/i;
         }
-    
-        int minSteps( int n ) 
-        {            
-            vvi dp(n+1,vi(n+1,0));
-            return ( n > 1 ? 1 + count(1,n,1,dp) : 0 );
-        }
-
+    }
+    return res;
+    }
 };
