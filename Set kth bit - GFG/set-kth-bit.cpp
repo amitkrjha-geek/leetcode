2c5bf9454ffd326f6cba59bfc8552d7f89d7eb45
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-07-10 23:41:13 +0530

    Added solution - LeetHub

---
+++ Set kth bit - GFG/set-kth-bit.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution
+{
+public:
+    int setKthBit(int n, int k)
+    {
+        // Write Your Code here
+        int a=1<<(k);
+        return n|a;
+    }
+    
+};
+
+
+
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N, K;
+        cin >> N >> K;
+        
+        Solution ob;
+        int ans = ob.setKthBit(N,K);
+        cout << ans << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
