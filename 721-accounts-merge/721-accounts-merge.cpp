c5b71613b2b31554b1cf0bca17aeab06744d74ac
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-08-23 16:09:48 +0530

    Time: 174 ms (62.15%), Space: 54.4 MB (12.42%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.cpp
+++ 721-accounts-merge/721-accounts-merge.cpp
@@ -1,67 +1,53 @@
 class Solution {
 public:
   
-    vector<int> parent, size;
-    int find_set(int v)
-    {
-        if(v == parent[v])
-            return v;
-        return parent[v] = find_set(parent[v]);
-    }
-    void union_set(int a, int b)
-    {
-        a = find_set(a);
-        b = find_set(b);
-        if(a == b) return;
-        if(size[a] < size[b])
-        {
-            parent[a] = b;
-            size[b] += size[a];
-        }
-        else
-        {
-            parent[b] = a;
-            size[a] += size[b];
-        }
-        return;
-    }
-public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        int n = accounts.size();
-        unordered_map<string, int> um;
-        int i = 0;
-        for(vector<string> &vec : accounts)
-        {
-            parent.push_back(i);
-            size.push_back(1);
-            for(int j = 1; j < vec.size(); j++)
-            {
-                if(um.count(vec[j]))
-                    union_set(i, um[vec[j]]);
-                else
-                    um[vec[j]] = i;
-            }
-            i++;
-        }
-        unordered_map<int, set<string>> us;  
-        for(int i = 0; i < n; i++)
-        {
-            int val = find_set(i);
-            for(int j = 1; j < accounts[i].size(); j++)
-            {
-                us[val].insert(accounts[i][j]);
-            }
-        }
-        vector<vector<string>> ans;
-        for(auto &[fst, snd] : us)
-        {
-            vector<string> tmp;
-            tmp.push_back(accounts[fst][0]);
-            for(string s : snd)
-                tmp.push_back(s);
-            ans.push_back(tmp);
-        }
-        return ans;
-    }
+   map<string, vector<string>> graph;
+unordered_set<string> visited;
+vector<vector<string>> res;
 
+void dfs(string source, vector<string>& temp)
+{
+    visited.insert(source);
+    
+    temp.push_back(source);
+    
+    for(auto x: graph[source])
+    {
+        if(!visited.count(x))
+        {
+            dfs(x,temp);
+        }
+    }
+}
+
+vector<vector<string>> accountsMerge(vector<vector<string>>& accounts)    {
+    
+    
+    for(auto list : accounts)
+    {
+        for(int i=1; i<list.size(); i++)
+        {
+            graph[list[1]].push_back(list[i]);
+            graph[list[i]].push_back(list[1]);
+        }
+    }
+    
+    for(auto list : accounts)
+    {
+        vector<string> temp;
+        
+        if(!visited.count(list[1]))
+        {
+            dfs(list[1], temp);
+            
+            sort(temp.begin(), temp.end());
+            
+            temp.insert(temp.begin(), list[0]);
+            
+            res.push_back(temp);
+        }
+    }
+    
+    return res;
+}
 };
