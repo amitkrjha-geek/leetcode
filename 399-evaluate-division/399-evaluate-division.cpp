db95123903f36e9d36433cacaa54351fc2ec2b67
Amit Kumar Jha <70425790+amitkrjha-geek@users.noreply.github.com>
2022-08-23 15:34:42 +0530

    Time: 3 ms (72.34%), Space: 8.4 MB (52.67%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,66 +1,38 @@
 class Solution {
 public:
-    void dfs(string start,string end,map<string,double>& mp,map<string,vector<string>>& graph,double& val,map<string,int>& visited,bool& found){
-        visited[start]=1;
     
-        if(found==true)
-            return ;
-        for(auto child:graph[start]){
-            if(visited[child]!=1){
-                // cout<<start<<" "<<child<<"\n";
-                val*=mp[start+"->"+child];
-                if(end==child){
-                    // cout<<end<<" -- "<<child<<"\n";
-                    found=true;
-                    return ;
-                }
-                dfs(child,end,mp,graph,val,visited,found);
-                if(found==true){
-                    return ;
-                }
-                else{
-                    val/=mp[start+"->"+child];
-                }
-            }
-        }
-       
-        
-    }
+      unordered_map<string, vector<pair<string, double>>> graph;
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        vector<double> ans;
-        map<string,double> mp;
-        map<string,vector<string>> graph;
-        for(int i=0;i<equations.size();i++){
-            string u=equations[i][0];
-            string v=equations[i][1];
-            mp[u+"->"+v]=values[i];
-            mp[v+"->"+u]=1/values[i];
-            graph[u].push_back(v);
-            graph[v].push_back(u);
+        
+        int n = equations.size();
+        for (int i = 0; i < n; i++) {
+            string a = equations[i][0];
+            string b = equations[i][1];
+            double val = values[i];
+            
+            graph[a].push_back({b, val});
+            graph[b].push_back({a, (double)1/val});
         }
-        for(int i=0;i<queries.size();i++){
-            string start=queries[i][0];
-            string end=queries[i][1];
-            if(graph.find(start)==graph.end()||graph.find(end)==graph.end()){
-                ans.push_back(-1);
-            }
-            else{
-                // ans.push_back(1);
-                double val=1;
-                map<string,int> visited;
-                bool found=false;
-                if(start==end){
-                    found=true;
-                }
-                else
-                dfs(start,end,mp,graph,val,visited,found);
-                if(found==true)
-                ans.push_back(val);
-                else
-                    ans.push_back(-1);
-                
-            }
+        
+        vector<double> result;
+        for (auto query : queries) {
+            unordered_set<string> visited;
+            result.push_back(dfs(query[0], query[1], visited));
         }
-        return ans;
+        return result;
     }
+    
+    double dfs(string src, string dst, unordered_set<string> &visited) {
+        if (graph.find(src) == graph.end()) return -1;
+        if (src == dst) return 1;
+        
+        for (auto node : graph[src]) {
+            if (visited.count(node.first)) continue;
+            visited.insert(node.first);
+            double res = dfs(node.first, dst, visited);
+            if (res != -1) return res * node.second;
+        }
+        return -1;
+    }
+
 };
